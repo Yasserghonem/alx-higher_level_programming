7f74c3e97646d9676af9630a07ee770d4fd510cc
Yasserghonems <yasserghonem56@gmail.com>
2023-05-11 12:59:54 -0700

    all the six tasks

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+from calculator_1 import (
+        add, sub, mul, div
+)
+
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     c = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(c, b, add(c, b)))
-    print("{:d} - {:d} = {:d}".format(c, b, sub(c, b)))
-    print("{:d} * {:d} = {:d}".format(c, b, mul(c, b)))
-    print("{:d} / {:d} = {:d}".format(c, b, div(c, b)))
+    print("{} + {} = {}".format(c, b, add(c, b)))
+    print("{} - {} = {}".format(c, b, sub(c, b)))
+    print("{} * {} = {}".format(c, b, mul(c, b)))
+    print("{} / {} = {}".format(c, b, div(c, b)))
