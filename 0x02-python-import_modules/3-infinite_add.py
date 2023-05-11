a7016183651e85766934006c04489a2ae74d32ed
Yasserghonems <yasserghonem56@gmail.com>
2023-05-11 13:02:42 -0700

    all the six tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-p = 0
-for n in argv[1:]:
-    p += int(n)
-print("{:d}".format(p))
+import sys
+
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    sum = 0
+    for x in argv:
+        sum += int(x)
+    print("{}".format(sum))
